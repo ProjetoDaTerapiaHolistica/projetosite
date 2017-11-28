cebe75bb73f976226cf60fed204ab38caeb5a11e
André Pinto <goldvasser@gmail.com>
2017-11-28 20:44:18 +0000

    Adicionada a rota QuemSomosR, criado conteudoQuemSomos. - João Vitor

--- src/Handler/Pagina.hs
+++ src/Handler/Pagina.hs
@@ -25,8 +25,8 @@ getHomeR = defaultLayout $ do
       <meta name="description" content="Site da Terapia Holística feito na framework Yesod">
     |]
 
-    let navbarfixed = "navbar-transparent navbar-fixed-top" :: Text
-    let colorOnScroll = "color-on-scroll=200" :: Text
+    -- let navbarfixed = "navbar-transparent navbar-fixed-top" :: Text
+    -- let colorOnScroll = "color-on-scroll=200" :: Text
     $(whamletFile "templates/menunav.hamlet")
     $(whamletFile "templates/conteudoindex.hamlet")
     $(whamletFile "templates/footer.hamlet")
@@ -73,13 +73,28 @@ getEspecialistaR = defaultLayout $ do
     $(whamletFile "templates/footer.hamlet")
 
 
+getQuemSomosR :: Handler Html
+getQuemSomosR = defaultLayout $ do
+    addStylesheet $ StaticR css_bootstrap_css
+    addStylesheet $ StaticR css_gaia_css
+    addScript $ StaticR js_jquery_min_js
+    addScript $ StaticR js_modernizr_js
+    addScript $ StaticR js_bootstrap_js
+    addScript $ StaticR js_gaia_js
+    addStylesheetRemote "https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600"
+    
+    toWidgetHead $ [hamlet|
+      <meta charset="UTF-8">
+      <meta name="description" content="Site da Terapia Holística feito na framework Yesod">
+    |]
+    $(whamletFile "templates/menunav.hamlet")
+    $(whamletFile "templates/conteudoQuemSomos.hamlet")
+    $(whamletFile "templates/footer.hamlet")
 
 getAgendarR :: Handler Html
 getAgendarR = undefined
 getContatoR :: Handler Html
 getContatoR = undefined
-getSobreR :: Handler Html
-getSobreR = undefined
     
 
 
