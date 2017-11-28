0a66c21b7838660106d5e770e0a6feca407d2f8f
André Pinto <goldvasser@gmail.com>
2017-11-28 01:52:35 +0000

    Novas rotas: TratamentoR e EspecialistaR. Adicionado interpoladores para classes do menunav.hamlet. - João Vitor

--- src/Handler/Pagina.hs
+++ src/Handler/Pagina.hs
@@ -16,20 +16,63 @@ getHomeR = defaultLayout $ do
     addStylesheet $ StaticR css_gaia_css
     addScript $ StaticR js_jquery_min_js
     addScript $ StaticR js_modernizr_js
-    addScript $ StaticR js_gaia_js
     addScript $ StaticR js_bootstrap_js
+    addScript $ StaticR js_gaia_js
     addStylesheetRemote "https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600"
-            --toWidgetHead $ $(juliusFile "templates/js/jquery.dropotron.min.julius")
     
     toWidgetHead $ [hamlet|
       <meta charset="UTF-8">
       <meta name="description" content="Site da Terapia Holística feito na framework Yesod">
-      <meta name="keywords" content="Terapia, Holistica, Tratamento, Agendar">
     |]
-    
+
+    let navbarfixed = "navbar-transparent navbar-fixed-top" :: Text
+    let colorOnScroll = "color-on-scroll=200" :: Text
     $(whamletFile "templates/menunav.hamlet")
     $(whamletFile "templates/conteudoindex.hamlet")
     $(whamletFile "templates/footer.hamlet")
+
+
+
+
+getTratamentoR :: Handler Html
+getTratamentoR = defaultLayout $ do
+    addStylesheet $ StaticR css_bootstrap_css
+    addStylesheet $ StaticR css_gaia_css
+    addScript $ StaticR js_jquery_min_js
+    addScript $ StaticR js_modernizr_js
+    addScript $ StaticR js_bootstrap_js
+    addScript $ StaticR js_gaia_js
+    addStylesheetRemote "https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600"
+    
+    toWidgetHead $ [hamlet|
+      <meta charset="UTF-8">
+      <meta name="description" content="Site da Terapia Holística feito na framework Yesod">
+    |]
+    $(whamletFile "templates/menunav.hamlet")
+    $(whamletFile "templates/conteudoTratamento.hamlet")
+    $(whamletFile "templates/footer.hamlet")
+
+
+
+getEspecialistaR :: Handler Html
+getEspecialistaR = defaultLayout $ do
+    addStylesheet $ StaticR css_bootstrap_css
+    addStylesheet $ StaticR css_gaia_css
+    addScript $ StaticR js_jquery_min_js
+    addScript $ StaticR js_modernizr_js
+    addScript $ StaticR js_bootstrap_js
+    addScript $ StaticR js_gaia_js
+    addStylesheetRemote "https://fonts.googleapis.com/css?family=Cambo|Poppins:400,600"
+    
+    toWidgetHead $ [hamlet|
+      <meta charset="UTF-8">
+      <meta name="description" content="Site da Terapia Holística feito na framework Yesod">
+    |]
+    $(whamletFile "templates/menunav.hamlet")
+    $(whamletFile "templates/conteudoEspecialista.hamlet")
+    $(whamletFile "templates/footer.hamlet")
+
+
 
 getAgendarR :: Handler Html
 getAgendarR = undefined
